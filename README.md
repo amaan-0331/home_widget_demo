319d3d1f5eb192c3c5318033f049a32987660dfd
Amaan Ansari <masteramaan0331@gmail.com>
2024-07-11 15:13:30 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# home_widget_demo
+
+A new Flutter project.
---
+++ ios/NewsWidgets/NewsWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NewsWidgetsBundle.swift
+//  NewsWidgets
+//
+//  Created by Amaan on 11/07/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NewsWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        NewsWidgets()
+    }
+}
